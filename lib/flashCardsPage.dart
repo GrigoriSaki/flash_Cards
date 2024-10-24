1e4525708ba49e9d8d5147cfe49689d736713835
Grzegorz Wisniowski <grzesiekwis200@gmail.com>
2024-10-24 19:01:33 +0200

    dodanie sliverappbar

--- lib/flashCardsPage.dart
+++ lib/flashCardsPage.dart
@@ -1,4 +1,6 @@
-// ignore_for_file: prefer_const_constructors, prefer_final_fields, must_be_immutable, unused_import
+// ignore_for_file: prefer_const_constructors, prefer_final_fields, must_be_immutable, unused_import, prefer_const_literals_to_create_immutables
+
+import 'dart:math';
 
 import 'package:flash_cards/homePage.dart';
 import 'package:flutter/material.dart';
@@ -18,19 +20,50 @@ class _FLashCardsPageState extends State<FLashCardsPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        iconTheme: IconThemeData(color: Color(0xff0ec298)),
-        backgroundColor: Color(0xFF15142e),
-        title: Text(
-          widget.folderDesc[widget.indexList],
-          style: GoogleFonts.inter(
-              fontSize: 22,
-              fontWeight: FontWeight.w500,
-              color: Color(0xff0ec298)),
-        ),
-      ),
-      body: Container(
-        color: Color(0xFF15142e),
+      body: CustomScrollView(
+        slivers: <Widget>[
+          SliverAppBar(
+            iconTheme: IconThemeData(color: Color(0xff0ec298)),
+            floating: true,
+            expandedHeight: 100,
+            backgroundColor: Color(0xFF15142e),
+            flexibleSpace: FlexibleSpaceBar(
+              background: Column(
+                children: [
+                  SizedBox(
+                    height: 25,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 35.0),
+                    child: Text(
+                      "Learn please!",
+                      style: GoogleFonts.sixtyfour(
+                          backgroundColor: Colors.white.withOpacity(0.2),
+                          color: Colors.white,
+                          fontSize: 22),
+                    ),
+                  ),
+                ],
+              ),
+              title: Padding(
+                padding: const EdgeInsets.only(top: 35.0, right: 20),
+                child: Text(
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                  widget.folderDesc[widget.indexList],
+                  style: GoogleFonts.inter(color: Color(0xff0ec298)),
+                ),
+              ),
+            ),
+          ),
+          SliverList(
+              delegate:
+                  SliverChildBuilderDelegate((BuildContext context, int index) {
+            return ListTile(
+              title: Text("mama"),
+            );
+          }, childCount: 15))
+        ],
       ),
     );
   }
